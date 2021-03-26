bcdd2d9e850e6bc50f46925dadb75a7ae026ebb7
Adam Poplawski <Adam.Poplawski@Acelity.com>
2021-03-26 16:49:45 +0000

    code refactoring

--- db_connection.php
+++ db_connection.php
@@ -7,7 +7,6 @@
 		$database = new PDO("mysql:host={$config['host']};dbname={$config['database']};charset=utf8", $config['user'], $config['password'],
 		[PDO::ATTR_EMULATE_PREPARES => false, 
 		 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-		unset($_SESSION['server_error']);
 		 
 	} catch (PDOException $error) {
 		
